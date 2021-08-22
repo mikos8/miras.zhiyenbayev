26d89bc31cd80544074cf0ee9d5989f6c025c654
mikos8 <mikos.zh8@gmail.com>
2021-08-22 22:39:45 +0600

    section popup portfolio

--- js/script.js
+++ js/script.js
@@ -1,24 +1,42 @@
-const btntabExp = document.getElementById('experience');
-const btntabEdu = document.getElementById('education');
-const tabExp = document.getElementById('tabexp');
-const tabEdu = document.getElementById('tabedu');
+// about section
+const btntabExp = document.getElementById("experience");
+const btntabEdu = document.getElementById("education");
+const tabExp = document.getElementById("tabexp");
+const tabEdu = document.getElementById("tabedu");
 
-btntabExp.addEventListener('click', () => {
-    console.log('click exp', btntabExp, tabExp);
+btntabExp.addEventListener("click", () => {
+  console.log("click exp", btntabExp, tabExp);
 
-    btntabExp.classList.add('active');
-    tabExp.classList.add('active');
+  btntabExp.classList.add("active");
+  tabExp.classList.add("active");
 
-    btntabEdu.classList.remove('active');
-    tabEdu.classList.remove('active');
+  btntabEdu.classList.remove("active");
+  tabEdu.classList.remove("active");
 });
 
-btntabEdu.addEventListener('click', () => {
-    console.log('click edu');
+btntabEdu.addEventListener("click", () => {
+  console.log("click edu");
 
-    btntabEdu.classList.add('active');
-    tabEdu.classList.add('active');
+  btntabEdu.classList.add("active");
+  tabEdu.classList.add("active");
 
-    btntabExp.classList.remove('active');
-    tabExp.classList.remove('active');
+  btntabExp.classList.remove("active");
+  tabExp.classList.remove("active");
 });
+
+// portfolio section
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("view-project-btn")) {
+    togglePortfolioPopup();
+  }
+});
+
+document
+  .querySelector(".popup-close")
+  .addEventListener("click", togglePortfolioPopup);
+
+function togglePortfolioPopup() {
+  document.querySelector(".portfolio-popup").classList.toggle("open");
+  document.body.classList.toggle("hide-scrolling");
+  document.querySelector(".main").classList.toggle("hide");
+}
